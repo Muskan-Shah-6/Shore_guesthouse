7e13237c19a23ff78b89d6a7e4f44c5da680256e
muskiofficially <shahmadarmuskan48@gmail.com>
2025-06-18 12:28:08 +0530

    Muskan
    Project created using vite and tailwind css added

---
+++ shore_guestHouse/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react' 
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      <h1 className='text-2xl animate-bounce text-center text-green-800 mt-5 font-bold underline'>Assalamualaikum...</h1>
+    </>
+  )
+}
+
+export default App
