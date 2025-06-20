d8abdf4d59d746407b047ce2b8eb38b6b64202f2
muskiofficially <shahmadarmuskan48@gmail.com>
2025-06-20 18:56:57 +0530

    Muskan
    Added the grid for view gallery

--- shore_guestHouse/src/pages/Gallery.jsx
+++ shore_guestHouse/src/pages/Gallery.jsx
@@ -7,6 +7,7 @@ import viewImage3 from '../assets/images/Gallery/welcome_pg_3.png'
 import viewImage4 from '../assets/images/Gallery/welcome_pg_4.png'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import Footer from '../components/navigation/Footer'
 
 const Gallery = () => {
     useEffect(() => {
@@ -32,30 +33,52 @@ const Gallery = () => {
             <section className='py-30 overflow-hidden'>
                 <div className='text-center space-y-3'>
                     <p className='text-md text-black'>Exterior Views</p>
-                    <h1 className='text-5xl text-black'>Welcome to Shore Guesthouse</h1>
+                    <h1 className='text-xl lg:text-5xl sm:text-2xl text-black'>Welcome to Shore Guesthouse</h1>
                 </div>
-                <div className='grid grid-cols-2 mt-25 p-35 gap-5'>
-                    <div className='text-start' data-aos="fade-up">
-                        <img src={viewImage1} alt="first_img" height='55%' width='75%' />
-                        <p className='text-black text-start mt-3'>A heaven lies behind the blue door</p>
-                    </div>
-                    <div className='text-start' data-aos="fade-up">
-                        <img src={viewImage2} alt="first_img" height='85%%' width='55%' />
-                        <p className='text-black text-start mt-3'>A lodge Room</p>
-                    </div>
-                    <div className='text-start -mt-25'  data-aos="fade-up ">
-                        <img src={viewImage4} alt="first_img"  height='85%%' width='55%' />
-                        <p className='text-black text-start mt-3'>The outer Deck</p>
-                    </div>
-                    <div className='text-start' data-aos="fade-up">
-                        <img src={viewImage3} alt="first_img" height='55%' width='75%' />
-                        <p className='text-black text-start mt-3'>The outer Deck</p>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-20 px-4 sm:px-10 md:px-20">
+                    <div className="text-start" data-aos="fade-up">
+                        <img
+                            src={viewImage1}
+                            alt="first_img"
+                            className="w-[75%] md:w-[75%] mx-auto md:mx-0 transition-transform duration-300 hover:scale-105 shadow"
+                        />
+                        <p className="text-black mt-3">A haven lies behind the blue door</p>
                     </div>
 
+                    <div className="text-start" data-aos="fade-up">
+                        <img
+                            src={viewImage2}
+                            alt="second_img"
+                            className="w-[55%] md:w-[55%] mx-auto md:mx-0 transition-transform duration-300 hover:scale-105 shadow"
+                        />
+                        <p className="text-black mt-3">A lodge Room</p>
+                    </div>
+
+                    <div className="text-start -mt-96 md:mt-0" data-aos="fade-up">
+                        <img
+                            src={viewImage4}
+                            alt="third_img"
+                            className="w-[55%] md:w-[55%] mx-auto md:mx-0 transition-transform duration-300 hover:scale-105 shadow"
+                        />
+                        <p className="text-black mt-3">The outer Deck</p>
+                    </div>
+
+                    <div className="text-start" data-aos="fade-up">
+                        <img
+                            src={viewImage3}
+                            alt="fourth_img"
+                            className="w-[75%] md:w-[75%] mx-auto md:mx-0 transition-transform duration-300 hover:scale-105 shadow"
+                        />
+                        <p className="text-black mt-3">The outer Deck</p>
+                    </div>
                 </div>
+
             </section>
 
-
+            {/* Footer section */}
+            <section className='mt-1'>
+                <Footer />
+            </section>
         </>
     )
 }
